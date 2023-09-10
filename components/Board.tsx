073dc976c38bc4a1f6f1a3f9c94c37524f8a33e3
Andrew Nguyen <anguyen0615@gmail.com>
2023-09-10 00:17:14 -0500

    Update Board.tsx
    - Updated board component to handle DnD between same/different boards

--- components/Board.tsx
+++ components/Board.tsx
@@ -4,6 +4,7 @@ import { useBoardStore } from '@/store/BoardStore';
 import React, { useEffect } from 'react'
 import { DragDropContext, DropResult, Droppable } from 'react-beautiful-dnd';
 import Column from './Column';
+import { start } from 'repl';
 
 function Board() {
     const [board, getBoard, setBoardState] = useBoardStore((state) => [
@@ -31,6 +32,61 @@ function Board() {
             setBoardState({
                 ...board, columns: rearrangedColumns
             });
+        }
+
+        // This step is needed as the indexes are stored as numbers instead of id's with DND lib
+        const columns = Array.from(board.columns);
+        const startColIndex = columns[Number(source.droppableId)];
+        const finishColIndex = columns[Number(destination.droppableId)];
+
+        const startCol: Column = { 
+            id: startColIndex[0],
+            todos: startColIndex[1].todos,
+        };
+
+        const finishCol: Column = { 
+            id: finishColIndex[0],
+            todos: finishColIndex[1].todos,
+        }
+
+        if (!startCol || !finishCol) return;
+
+        if (source.index === destination.index && startCol === finishCol) return;
+
+        const newTodos = startCol.todos;
+        const [todoMoved] = newTodos.splice(source.index, 1);
+
+        if (startCol.id === finishCol.id) {
+            // Same column task drag
+            newTodos.splice(destination.index, 0 , todoMoved);
+            const newCol = { 
+                id: startCol.id,
+                todos: newTodos,
+            };
+            const newColumns = new Map(board.columns);
+            newColumns.set(startCol.id, newCol);
+
+            setBoardState({...board, columns: newColumns });
+        } else {
+            // Dragging to different column
+            const finishTodos = Array.from(finishCol.todos);
+            finishTodos.splice(destination.index, 0, todoMoved);
+            
+            const newColumns = new Map(board.columns);
+            const newCol = { 
+                id: startCol.id,
+                todos: newTodos,
+            };
+
+            newColumns.set(startCol.id, newCol);
+            newColumns.set(finishCol.id, {
+                id: finishCol.id,
+                todos: finishTodos,
+            });
+
+            // update DB
+
+            setBoardState({ ...board, columns: newColumns});
         }
     };
 

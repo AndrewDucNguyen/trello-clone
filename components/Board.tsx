afc44b50cf20728b9ba88728323dd6c8f48528e7
Andrew Nguyen <anguyen0615@gmail.com>
2023-09-12 18:39:51 -0500

    Implementing openAI summary
    - Created openai key and functions
    - Implemented summary function and structure
    - Presented on the board

--- components/Board.tsx
+++ components/Board.tsx
@@ -98,7 +98,7 @@ function Board() {
         <Droppable droppableId='board' direction='horizontal' type='column'>
             {(provided) => (
                 <div
-                    className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auot'
+                    className='grid grid-cols-1 md:grid-cols-3 gap-5 max-w-7xl mx-auto'
                     {...provided.droppableProps}
                     ref={provided.innerRef}
                 >
